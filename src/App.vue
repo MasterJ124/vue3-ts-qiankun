1d841f11c3d556cc5f42a2f58fa122c457bc66e9
jiyunjie <jiyunjie@netjoy.com>
2022-07-04 19:39:01 +0800

    页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,90 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <a-config-provider :locale="locale">
+    <a-layout>
+      <a-layout-header class="header">
+        <div class="logo" />
+        <a-menu
+          v-model:selectedKeys="selectedKeys1"
+          theme="dark"
+          mode="horizontal"
+          :style="{ lineHeight: '64px' }"
+        >
+          <a-menu-item key="1">nav 1</a-menu-item>
+          <a-menu-item key="2">nav 2</a-menu-item>
+          <a-menu-item key="3">nav 3</a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      <a-layout>
+        <a-layout-sider width="200" style="background: #fff">
+        <a-menu
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
+        >
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span>
+                <user-outlined />
+                subnav 1
+              </span>
+            </template>
+            <a-menu-item key="1">option1</a-menu-item>
+            <a-menu-item key="2">option2</a-menu-item>
+            <a-menu-item key="3">option3</a-menu-item>
+            <a-menu-item key="4">option4</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <laptop-outlined />
+                subnav 2
+              </span>
+            </template>
+            <a-menu-item key="5">option5</a-menu-item>
+            <a-menu-item key="6">option6</a-menu-item>
+            <a-menu-item key="7">option7</a-menu-item>
+            <a-menu-item key="8">option8</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+        <a-layout style="padding: 0 24px 24px">
+          <a-layout-content>
+            <div id="nav">
+              <router-link to="/">Home</router-link> |
+              <router-link to="/about">About</router-link>
+            </div>
+            <router-view />
+          </a-layout-content>
+        </a-layout>
+      </a-layout>
+    </a-layout>
+  </a-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
+
+export default defineComponent({
+  setup() {
+    return {
+      selectedKeys1: ref<string[]>(['2']),
+      selectedKeys2: ref<string[]>(['1']),
+      collapsed: ref<boolean>(false),
+      openKeys: ref<string[]>(['sub1']),
+    }
+  },
+  data(){
+    return{
+      locale: zhCN
+    }
+  }
+})
+</script>
 
 <style lang="scss">
 #app {
@@ -13,6 +93,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 
 #nav {
@@ -26,5 +107,20 @@
       color: #42b983;
     }
   }
+}
+.ant-layout{
+  height: 100%;
+}
+
+.logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.site-layout-background {
+  background: #fff;
 }
 </style>
